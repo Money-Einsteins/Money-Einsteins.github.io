ee8781c720102dfcba948e973122ada752b329a8
Money-Einsteins <136909740+Money-Einsteins@users.noreply.github.com>
2023-06-19 18:24:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+// Add to cart functionality
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const cartItemsContainer = document.getElementById('cart-items');
+
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const productDetails = button.parentElement;
+    const productImage = productDetails.querySelector('.product-image').src;
+    const productTitle = productDetails.querySelector('.product-title').textContent;
+    const productPrice = productDetails.querySelector('.product-price').textContent;
+
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item');
+    cartItem.innerHTML = `
+      <img src="${productImage}" alt="Product Image">
+      <div class="product-info">
+        <h3 class="product-title">${productTitle}</h3>
+        <p class="product-price">${productPrice}</p>
+      </div>
+      <button class="remove-from-cart">Remove</button>
+    `;
+
+    cartItemsContainer.appendChild(cartItem);
+  });
+});
